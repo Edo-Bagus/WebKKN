763aefa5a57af05eb39bd67c12f13ba409d6025d
EdoBagus <79005016+Edo-Bagus@users.noreply.github.com>
2025-08-29 17:01:42 +0700

    finall

--- web-kkn/src/components/ui/member-card.tsx
+++ web-kkn/src/components/ui/member-card.tsx
@@ -6,12 +6,6 @@ export default function MemberCard({ article }: any) {
   const author = article.author
   const videoRef = useRef<HTMLVideoElement>(null)
 
-  // // Pause video on mount just in case
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     videoRef.current.pause()
-  //   }
-  // }, [])
 
   return (
     <div className="col-span-1">
@@ -23,7 +17,7 @@ export default function MemberCard({ article }: any) {
             src={author.pictureUrl || '/videos/default.mp4'}
             className="rounded-full object-cover w-full h-full"
             autoPlay
-            muted
+            playsInline            muted
             loop
           />
         </div>
